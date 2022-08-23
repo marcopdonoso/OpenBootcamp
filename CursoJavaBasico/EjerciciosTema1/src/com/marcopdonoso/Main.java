0a60e78bebd2db640e771d2c1c1ff2d80f27b495
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-08-23 17:18:44 -0400

    Ejercicios del Tema 1

---
+++ CursoJavaBasico/EjerciciosTema1/src/com/marcopdonoso/Main.java
@@ -0,0 +1,38 @@
+package com.marcopdonoso;
+
+public class Main {
+    public static void main(String[] args) {
+        // Tipos de datos Primitivos
+
+        // Números enteros
+        byte number1 = 127;
+        short number2 = 32767;
+        int number3 = 2147483647;
+        long number4 = 9223372036854775807L;
+
+        // Números decimales
+        float number5 = 3.12987f;
+        double number6 = 5.2321;
+
+        // Booleano
+        boolean booleano1 = true;
+
+        // Carácteres
+        char caract1 = 'M';
+
+        // La clase String
+        String string1 = "Hola Mundo!";
+
+        // Imprimir los valores de los distintos tipos de datos primitivos y la clase String
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+        System.out.println(booleano1);
+        System.out.println(caract1);
+        System.out.println(string1);
+    }
+}
