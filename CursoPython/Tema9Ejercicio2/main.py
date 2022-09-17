9515ded456e798ea1fd7bbb282066940fd1cde36
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-09-17 01:11:59 -0400

    Correccion para poder imprimir la lista de numeros impares antes de la sumatoria

--- CursoPython/Tema9Ejercicio2/main.py
+++ CursoPython/Tema9Ejercicio2/main.py
@@ -2,8 +2,8 @@ from functools import reduce
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-impares = filter(lambda x: x % 2 != 0, lista)
+impares = list(filter(lambda x: x % 2 != 0, lista))
+print(impares)
 
 sumaImpares = reduce(lambda x, y: x + y, impares)
-
 print(sumaImpares)
