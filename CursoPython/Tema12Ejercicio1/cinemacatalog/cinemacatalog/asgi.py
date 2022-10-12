27605323263a1a6d2be511328c41c4e379296224
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-10-12 16:18:41 -0400

    Elimino carpeta de prueba y creo proyecto Django: cinemacatalog y la app catalog, ademas registro la app en el proyecto (settings: installed_apps)

--- CursoPython/Tema12Ejercicio1/cinemacatalog/cinemacatalog/asgi.py
+++ CursoPython/Tema12Ejercicio1/cinemacatalog/cinemacatalog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mytestsite project.
+ASGI config for cinemacatalog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinemacatalog.settings')
 
 application = get_asgi_application()
