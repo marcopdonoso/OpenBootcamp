9f0ab6840a9d2c16c0dac3f4733e5cc32ba4ee92
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-10-20 10:59:23 -0400

    Registro de modelos en admin.py, creacion de superuser para ingresar al sitio de administracion de Django, registro de directores y peliculas de prueba

--- CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/models.py
+++ CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/models.py
@@ -12,7 +12,7 @@ class Director(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-    
+
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 
