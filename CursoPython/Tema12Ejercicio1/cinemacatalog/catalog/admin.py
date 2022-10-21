0f2660e6daddbb619fda05f0e39a70a8cc6ca643
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-10-21 10:50:56 -0400

    Configuracion mejorada del sitio de administracion de Django

--- CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/admin.py
+++ CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/admin.py
@@ -3,5 +3,22 @@ from .models import Director, Movie
 
 # Register your models here.
 
-admin.site.register(Director)
-admin.site.register(Movie)
+# admin.site.register(Director)
+# admin.site.register(Movie)
+
+
+class MovieInline(admin.TabularInline):
+    model = Movie
+    extra = 0
+
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [MovieInline]
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'description')
