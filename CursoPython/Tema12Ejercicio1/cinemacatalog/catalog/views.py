788cf45f92796807a793484394ff87ad984f139c
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-10-24 10:59:32 -0400

    Creacion de pagina de inicio para el sitio, la pagina despliega algunos conteos de registros de la base de datos y contiene enlaces a paginas aun no creadas.

--- CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/views.py
+++ CursoPython/Tema12Ejercicio1/cinemacatalog/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Movie, Director
 
 # Create your views here.
+
+
+def index(request):
+    num_movies = Movie.objects.count()
+    num_directors = Director.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_movies': num_movies, 'num_directors': num_directors},
+    )
